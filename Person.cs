ac10c1014fdd7571baa971f6b65197f29060fe21
Thomas Paul <tpaul19@wgu.edu>
2020-02-06 20:41:38 -0600

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace PersonGenerator
+{
+    public class Person
+    {
+        private readonly string name;
+        private readonly bool isMale;
+        private readonly int age;
+        private string address;
+        private string emailAddress;
+        private string cellPhone;
+
+        public Person()
+            {}
+
+        public Person (string name, bool isMale, int age, string address, string emailAddress, string cellPhone)
+        {
+            this.name = name;
+            this.isMale = isMale;
+            this.age = age;
+            this.address = address;
+            this.emailAddress = emailAddress;
+            this.cellPhone = cellPhone;
+        }
+
+        public string getName()
+        {
+            
+        }
+    }
+}
+
+
+// name
+// gender
+// age
+// address
+// emailAddress
+// cellPhone
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PersonGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+
+//1. ask user if they want to generate males, females, or both
+//2. ask the user how many they want to generate
+//3. ask the user if they want to print to console
+//4. ask the user if they want to write results to file(ask for input)
+//5. return the full path to file
+
+//versionA: print results to console
+//versionB: offer to store results in file
+//versionC: ask for file paths to name for male, female, last names
+//versionD: use console application
+//versionE: use sqlite database to store(with LINQ)
