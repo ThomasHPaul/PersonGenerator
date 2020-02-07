421a986a7d552665156ad632d5ed645ccff1e91b
ThomasHPaul <tpaul19@wgu.edu>
2020-02-07 15:51:15 -0600

    Added name text files. Added stream readers.

--- Program.cs
+++ Program.cs
@@ -1,17 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace PersonGenerator
 {
     class Program
     {
+
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string name;
+            StreamReader sr;
+            List<string> femaleFirstNames = new List<string>();
+            List<string> lastNames = new List<string>();
+            List<string> maleFirstNames = new List<string>();
 
+            string femaleFirstNamesPath = @"C:\Users\TPaul9\source\repos\PersonGenerator\data\femaleFirstNames.txt";
+            string lastNamesPath = @"C:\Users\TPaul9\source\repos\PersonGenerator\data\lastNames.txt";
+            string maleFirstNamesPath = @"C:\Users\TPaul9\source\repos\PersonGenerator\data\maleFirstNames.txt";
 
-            string[] lastNames = new string[10];
-            string[] maleFirstNames = new string[10];
-            string[] femailFirstNames = new string[10];
+            using (sr = new StreamReader(femaleFirstNamesPath))
+            {
+                while ((name = sr.ReadLine()) != null)
+                {
+                    femaleFirstNames.Add(name);
+                }
+            }
+
+            using (sr = new StreamReader(lastNamesPath))
+            {
+                while ((name = sr.ReadLine()) != null)
+                {
+                    lastNames.Add(name);
+                }
+            }
+
+            using (sr = new StreamReader(maleFirstNamesPath))
+            {
+                while ((name = sr.ReadLine()) != null)
+                {
+                    maleFirstNames.Add(name);
+                }
+            }
         }
     }
 }
